2e6d54863499c681ad6714642cd1a24b4605ba38
phanthanhcongo <cong.pt.2412@aptechlearning.edu.vn>
2026-06-29 11:41:37 +0700

    Update 2026-06-12-add-erp-outbox-view.sql

--- directus/sql/migrations/2026-06-12-add-erp-outbox-view.sql
+++ directus/sql/migrations/2026-06-12-add-erp-outbox-view.sql
@@ -4,9 +4,7 @@ CREATE INDEX IF NOT EXISTS integration_events_status_next_attempt_idx
 CREATE INDEX IF NOT EXISTS integration_events_entity_record_idx
   ON integration_events (entity, record_id);
 
-DROP VIEW IF EXISTS failed_erp_webhooks;
-
-CREATE VIEW failed_erp_webhooks AS
+CREATE OR REPLACE VIEW failed_erp_webhooks AS
 SELECT *
 FROM integration_events
 WHERE status = 'failed';
